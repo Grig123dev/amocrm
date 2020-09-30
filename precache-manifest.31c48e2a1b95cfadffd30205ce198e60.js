cab916fc1819349506d5b341805e586f8baab738
Grig <g.sargsyan92@gmail.comgit>
2020-09-30 19:33:03 +0400

    first commit

---
+++ precache-manifest.31c48e2a1b95cfadffd30205ce198e60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a2d547c2c8f773ccaad656358e63e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7afa2b1ebc4d05086fe7",
+    "url": "/static/css/main.fa6be0f4.chunk.css"
+  },
+  {
+    "revision": "d74ec1e8ee16407c68f0",
+    "url": "/static/js/2.224a2b1e.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.224a2b1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7afa2b1ebc4d05086fe7",
+    "url": "/static/js/main.e386dada.chunk.js"
+  },
+  {
+    "revision": "8d2955730fd9e8b2f878",
+    "url": "/static/js/runtime-main.fe1276f6.js"
+  }
+]);
